9a3057933eb8d8bc2d08e60e42c880e68b0088ad
Kasper van der Heijden <kasper@kaspervdheijden.nl>
2020-05-16 18:46:53 +0200

    Small changes

--- bin/pipeline.sh
+++ bin/pipeline.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env sh
 
 dirName=$(git rev-parse --show-toplevel 2>/dev/null);
-if [ -z "${dirName}" ]; then
+if [ -z "${dirName}" ] || [ ! -d "${dirName}" ]; then
   echo 'Not a git repository' >&2;
   exit 5;
 fi
 
 echo 'Checking for PHP syntax errors...';
-output=$(find "${dirName}" -type f -name '*.php' -and ! -wholename '*/vendor/*' -exec php -l {} \; 2>&1 | grep 'Errors parsing ' | cut -c16-);
+
+phpFiles=$(find "${dirName}" -type f -name '*.php' -and ! -wholename '*/vendor/*' 2>/dev/null);
+output=$(echo "${phpFiles}" | grep 'Errors parsing ' | cut -c16-);
 if [ -n "${output}" ]; then
   echo "${output}" | xargs -I{} echo ' - {}' >&2;
   exit 6;
 fi
 
 echo 'Checking for strict typed classes...';
-output=$(find "${dirName}" -type f -name '*.php' -and ! -wholename '*/vendor/*' -print0 2>/dev/null | xargs -0I{} -n1 grep -L 'declare(strict_types=1);' {} | xargs -I{} echo ' - {}');
+output=$(echo "${phpFiles}" | xargs -I{} -n1 grep -L 'declare(strict_types=1);' {} | xargs -I{} echo ' - {}');
 if [ -n "${output}" ]; then
   echo "${output}" >&2;
   exit 7;
 fi
 
 echo 'Checking console...';
-if ! "${dirName}/bin/console.php" '\Fugue\Command\TestCommand'; then
+if ! "${dirName}/bin/console" '\Fugue\Command\TestCommand'; then
   echo 'Console test failed!' >&2;
   exit 8;
 fi
